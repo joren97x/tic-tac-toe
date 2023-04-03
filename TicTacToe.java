f7b202a3def48c257fd0820bb94d2f4921323252
joren97x <babamyloves2002@gmail.com>
2023-04-03 11:33:51 -0400

    fixed scoreboard && added menu: restart and quit button

--- TicTacToe.java
+++ TicTacToe.java
@@ -15,6 +15,8 @@ public class TicTacToe implements ActionListener {
     JLabel scoreboard, Xscore, Oscore;
     JButton[] button = new JButton[9];
     JPanel panel;
+    JButton[] menuButton = new JButton[2];
+    JPanel menu;
     Font font = new Font("Monospaced", Font.BOLD, 30);
     int x = 0;
 
@@ -29,11 +31,11 @@ public class TicTacToe implements ActionListener {
         scoreboard.setFont(font);
         scoreboard.setBounds(110, -80, 400, 200);
 
-        Xscore = new JLabel("X: ");
+        Xscore = new JLabel("X: 0");
         Xscore.setFont(font);
         Xscore.setBounds(40, -35, 100, 200);
 
-        Oscore = new JLabel("O: ");
+        Oscore = new JLabel("O: 0");
         Oscore.setFont(font);
         Oscore.setBounds(270, -35, 100, 200);
 
@@ -43,22 +45,38 @@ public class TicTacToe implements ActionListener {
         winner.setText("HELLO");
         winner.setFont(font);
 
+        panel = new JPanel();
+        panel.setBounds(20,100,390,345);
+        panel.setBackground(new Color(100,200,50));
+        panel.setLayout(new GridLayout(3,3,2,2));
+
+        menu = new JPanel();
+        menu.setBounds(100, 150, 220,200);
+        menu.setBackground(new Color(34,34,34));
+        menu.setLayout(new GridLayout(2,1,5,5));
+
+        menuButton[0] = new JButton("Restart");
+        menuButton[1] = new JButton("Quit");
+
         for(int i = 0; i < button.length; i++) {
             button[i] = new JButton("");
             button[i].addActionListener(this);
             button[i].setFont(font);
         }
 
-        panel = new JPanel();
-        panel.setBounds(20,100,390,345);
-        panel.setBackground(new Color(100,200,50));
-        panel.setLayout(new GridLayout(3,3,2,2));
-        
+        for(int i = 0; i < menuButton.length; i++) {
+            menuButton[i].setFont(font);
+            menuButton[i].addActionListener(this);
+            menu.add(menuButton[i]);
+        }
+
         for(int i = 0; i < 9; i++) {
             panel.add(button[i]);
         }
 
+        menu.setVisible(false);
         frame.add(panel);
+        frame.add(menu);
         frame.add(winner);
         frame.add(scoreboard);
         frame.add(Xscore);
@@ -73,6 +91,7 @@ public class TicTacToe implements ActionListener {
         for(int i = 0; i <= 8; i++) {
             if(x%2==0 && e.getSource() == button[i]){
                 if(button[i].getText().equals("X") || button[i].getText().equals("O")) {
+                    //do nothing
                 }
                 else {
                     button[i].setText("X");
@@ -81,46 +100,155 @@ public class TicTacToe implements ActionListener {
             }
             if(x%2!=0 && e.getSource() == button[i]){
                 if(button[i].getText().equals("X") || button[i].getText().equals("O")) {
+                    //do nothing
                 }
                 else {
                     button[i].setText("O");
-                 this.x++;
+                    this.x++;
                 }
+            }
+        }
+
+        if(e.getSource() == menuButton[1]){
+            System.exit(0);
+        }
+
+        if(e.getSource() == menuButton[0]) {
+            for(int i = 0; i < 9; i++) {
+                button[i].setText("");
+                menu.setVisible(false);
+                panel.setVisible(true); 
+                this.x = 0;               
             }
         }
         
         if((button[0].getText()+button[3].getText().concat(button[6].getText())).equals("XXX")
         || (button[0].getText()+button[3].getText().concat(button[6].getText())).equals("OOO")) {
             winner.setText(button[0].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[0].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[1].getText()+button[4].getText().concat(button[7].getText())).equals("XXX")
         || (button[1].getText()+button[4].getText().concat(button[7].getText())).equals("OOO")) {
             winner.setText(button[1].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[1].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[2].getText()+button[5].getText().concat(button[8].getText())).equals("XXX")
         || (button[2].getText()+button[5].getText().concat(button[8].getText())).equals("OOO")) {
             winner.setText(button[2].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[2].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[0].getText()+button[1].getText().concat(button[2].getText())).equals("XXX")
         || (button[0].getText()+button[1].getText().concat(button[2].getText())).equals("OOO")) {
             winner.setText(button[0].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[0].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[3].getText()+button[4].getText().concat(button[5].getText())).equals("XXX")
         || (button[3].getText()+button[4].getText().concat(button[5].getText())).equals("OOO")) {
             winner.setText(button[3].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[3].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[6].getText()+button[7].getText().concat(button[8].getText())).equals("XXX")
         || (button[6].getText()+button[7].getText().concat(button[8].getText())).equals("OOO")) {
             winner.setText(button[6].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[6].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
-
         if((button[0].getText()+button[4].getText().concat(button[8].getText())).equals("XXX")
         || (button[0].getText()+button[4].getText().concat(button[8].getText())).equals("OOO")) {
             winner.setText(button[0].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[0].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
         if((button[6].getText()+button[4].getText().concat(button[2].getText())).equals("XXX")
         || (button[6].getText()+button[4].getText().concat(button[2].getText())).equals("OOO")) {
             winner.setText(button[6].getText()+" WINS!");
+            panel.setVisible(false);
+            menu.setVisible(true);
+            if(button[6].getText().equals("X")){
+                int score = Character.getNumericValue(Xscore.getText().charAt(3));
+                score++;
+                Xscore.setText("X: "+Integer.toString(score));
+            }
+            else {
+                int score = Character.getNumericValue(Oscore.getText().charAt(3));
+                score++;
+                Oscore.setText("O: "+Integer.toString(score));
+            }
         }
 
         throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
@@ -130,7 +258,10 @@ public class TicTacToe implements ActionListener {
     public static void main(String[] args){
         new TicTacToe();
         //
-        //create a function that checks if there are straight x's or o's
+        //create a function that checks if there are straight x's or o's == DONE!
+        //add menu functionalities == done
+        //fix scoreboard == DONE
+        //add AI enemy
 
     }
 }
